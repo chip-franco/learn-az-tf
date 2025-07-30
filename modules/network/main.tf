380b2fe4856fad1a7ad4c82483b171037e064844
chip97 <chip97@naver.com>
2025-07-30 21:19:37 +0900

    Update main.tf

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -9,4 +9,6 @@
         resource_group_name  = var.rg_name
         virtual_network_name = azurerm_virtual_network.vnet.name
         address_prefixes     = ["10.0.1.0/24"]
+
+        depends_on = [azurerm_virtual_network.vnet]
     }   
