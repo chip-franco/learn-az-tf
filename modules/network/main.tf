0e82cc7d69ce171e2dfcb70de68a0ab59b781f04
chip97 <chip97@naver.com>
2025-07-30 18:18:51 +0900

    Update main.tf

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,6 +1,6 @@
     resource "azurerm_virtual_network" "vnet" {
         name                = var.vnet_name
-        address_space       = ["10.0.0/16"]
+        address_space       = ["10.0.0.0/16"]
         location            = var.location
         resource_group_name = var.rg_name
     }
@@ -8,5 +8,5 @@
         name                 = var.subnet_name
         resource_group_name  = var.rg_name
         virtual_network_name = azurerm_virtual_network.vnet.name
-        address_prefixes     = ["10.0.1/24"]
+        address_prefixes     = ["10.0.1.0/24"]
     }   
